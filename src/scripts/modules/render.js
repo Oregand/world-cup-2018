ba7874d365c358a8f849f6dac7094e515319ce5d
David O'Regan <doregan@vsware.ie>
2018-07-02 21:59:02 +0100

    [home-story] Add base tables

--- src/scripts/modules/render.js
+++ src/scripts/modules/render.js
@@ -14,9 +14,33 @@ const renderEntries = (entriesItems) => {
           entriesItems.map(
             entriesItem => `
               <tr>
-                <td>${entriesItem.Name}</td>
-                <td>${entriesItem.Ref}</td>
-                <td>${entriesItem['Total Points']}</td>
+                <td>${entriesItem[""]}</td>
+                <td>${entriesItem.NAME}</td>
+                <td>${entriesItem['WINNER A']}</td>
+                <td>${entriesItem['WINNER B']}</td>
+                <td>${entriesItem['WINNER C']}</td>
+                <td>${entriesItem['WINNER D']}</td>
+                <td>${entriesItem['WINNER E']}</td>
+                <td>${entriesItem['WINNER F']}</td>
+                <td>${entriesItem['WINNER G']}</td>
+                <td>${entriesItem['WINNER H']}</td>
+                <td>${entriesItem['R UP A']}</td>
+                <td>${entriesItem['R UP B']}</td>
+                <td>${entriesItem['R UP C']}</td>
+                <td>${entriesItem['R UP D']}</td>
+                <td>${entriesItem['R UP E']}</td>
+                <td>${entriesItem['R UP F']}</td>
+                <td>${entriesItem['R UP G']}</td>
+                <td>${entriesItem['R UP H']}</td>
+                <td>${entriesItem['Q/Finals']}</td>
+                <td>${entriesItem['Q/Finals goals']}</td>
+                <td>${entriesItem['Semi/Final']}</td>
+                <td>${entriesItem['3rd Place']}</td>
+                <td>${entriesItem['Finalist']}</td>
+                <td>${entriesItem['1st Round Goals']}</td>
+                <td>${entriesItem['2nd Round Goals']}</td>
+                <td>${entriesItem['Tie Breaker']}</td>
+                <td>${entriesItem['Winner']}</td>
               </tr>`
           ).join('\n')
         }
