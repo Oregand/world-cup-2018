2f7cae4bb27670b717f2d6c16d747c73b4605959
David O'Regan <doregan@vsware.ie>
2018-07-03 09:34:34 +0100

    [home-story] Add base tables

--- server.js
+++ server.js
@@ -4,7 +4,4 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'dist')))
-  .set('dist', path.join(__dirname, 'dist'))
-  .set('view engine', 'html')
-  .get('/', (req, res) => res.render('/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
